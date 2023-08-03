fe20e0d5540d0efb82783dde4e07a7d39be24e8f
GenieUU <youjin8019@naver.com>
2023-08-03 20:33:03 +0900

    songInput_make5line

--- src/pages/Member4.jsx
+++ src/pages/Member4.jsx
@@ -7,14 +7,22 @@ To Do
 -플레이리스트 노래 목록 삭제 기능(+체크박스)
 */
 
-import React from 'react'
-import styled from "@emotion/styled"
+import React, { useState } from 'react';
+import styled from "@emotion/styled";
 import outerboxImage from "../images/outerbox.png";
 import myphotoImage from "../images/me.jpeg";
 import { useNavigate } from 'react-router-dom';
 
 function Member4() {
   const navigate = useNavigate();
+  
+  const [songs, setSongs] = useState([
+    { id: 1, songTitle: '', artistName: '' },
+    { id: 2, songTitle: '', artistName: '' },
+    { id: 3, songTitle: '', artistName: '' },
+    { id: 4, songTitle: '', artistName: '' },
+    { id: 5, songTitle: '', artistName: '' },
+  ]);
 
   const menuHome = () => {
     document.getElementById("navHome").style = "color: black; background-color: white;"
@@ -119,339 +127,346 @@ function Member4() {
                 <TitleSubtitle>TODAY CHOICE</TitleSubtitle>
               </WrapperRightBodyBottomTitle>
               <WrapperRIghtBodyBottomTable>
-                <tr style={{ width: '100%', height: '20px' }}>
-                  <TableHeader style={{ width: "8%", textAlign: "center" }}><input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} /></TableHeader>
-                  <TableHeader style={{ width: "12%", textAlign: "center" }}>번호</TableHeader>
-                  <TableHeader style={{ width: "55%", textAlign: "center" }}>곡명</TableHeader>
-                  <TableHeader style={{ width: "25%", textAlign: "center" }}>아티스트</TableHeader>
-                </tr>
-                <tr style={{ width: '100%', height: '20px' }}>
-                  <TableData style={{ width: "8%", textAlign: "center" }}><input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} /></TableData>
-                  <TableData style={{ width: "12%", textAlign: "center" }}>1</TableData>
-                  <TableData style={{ width: "55%", textAlign: "left" }}>Your Song Title 1</TableData>
-                  <TableData style={{ width: "25%", textAlign: "left" }}>Artist Name 1</TableData>
-                </tr>
-              <tr style={{ width: '100%', height: '20px' }}>
-                <TableData style={{ width: "8%", textAlign: "center" }}><input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} /></TableData>
-                <TableData style={{ width: "12%", textAlign: "center" }}>2</TableData>
-                <TableData style={{ width: "55%", textAlign: "left" }}></TableData>
-                <TableData style={{ width: "25%", textAlign: "left" }}></TableData>
+                <tbody>
+                  <tr style={{ width: '100%', height: '20px' }}>
+                    <TableHeader style={{ width: "8%", textAlign: "center" }}><input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} /></TableHeader>
+                    <TableHeader style={{ width: "12%", textAlign: "center" }}>번호</TableHeader>
+                    <TableHeader style={{ width: "55%", textAlign: "center" }}>곡명</TableHeader>
+                    <TableHeader style={{ width: "25%", textAlign: "center" }}>아티스트</TableHeader>
+                  </tr>
+                  {songs.map((song) => (
+              <tr key={song.id} style={{ width: '100%', height: '20px' }}>
+                <TableData style={{ width: "8%", textAlign: "center" }}>
+                  <input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} />
+                </TableData>
+                <TableData style={{ width: "12%", textAlign: "center" }}>{song.id}</TableData>
+                <TableData style={{ width: "55%", textAlign: "left" }}>
+                  <input
+                    type="text"
+                    value={song.songTitle}
+                    onChange={(e) => {
+                      const updatedSongs = songs.map((s) =>
+                        s.id === song.id ? { ...s, songTitle: e.target.value } : s
+                      );
+                      setSongs(updatedSongs);
+                    }}
+                    placeholder="Song Title"
+                  />
+                </TableData>
+                <TableData style={{ width: "25%", textAlign: "left" }}>
+                  <input
+                    type="text"
+                    value={song.artistName}
+                    onChange={(e) => {
+                      const updatedSongs = songs.map((s) =>
+                        s.id === song.id ? { ...s, artistName: e.target.value } : s
+                      );
+                      setSongs(updatedSongs);
+                    }}
+                    placeholder="Artist Name"
+                  />
+                </TableData>
               </tr>
-              <tr style={{ width: '100%', height: '20px' }}>
-                <TableData style={{ width: "8%", textAlign: "center" }}><input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} /></TableData>
-                <TableData style={{ width: "12%", textAlign: "center" }}>3</TableData>
-                <TableData style={{ width: "55%", textAlign: "left" }}></TableData>
-                <TableData style={{ width: "25%", textAlign: "left" }}></TableData>
-              </tr>
-              <tr style={{ width: '100%', height: '20px' }}>
-                <TableData style={{ width: "8%", textAlign: "center" }}><input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} /></TableData>
-                <TableData style={{ width: "12%", textAlign: "center" }}>4</TableData>
-                <TableData style={{ width: "55%", textAlign: "left" }}></TableData>
-                <TableData style={{ width: "25%", textAlign: "left" }}></TableData>
-              </tr>
-              <tr style={{ width: '100%', height: '20px' }}>
-                <TableData style={{ width: "8%", textAlign: "center" }}><input type="checkbox" name="" id="" style={{ width: "12px", height: "12px" }} /></TableData>
-                <TableData style={{ width: "12%", textAlign: "center" }}>5</TableData>
-                <TableData style={{ width: "55%", textAlign: "left" }}></TableData>
-                <TableData style={{ width: "25%", textAlign: "left" }}></TableData>
-              </tr>
-            </WrapperRIghtBodyBottomTable>
-          </WrapperRightBodyBottom>
-        </WrapperRightBody>
-      </WrapperRight>
-      <Navigation>
-        <NavigationItem id="navHome" onClick={() => {
-          navigate("/");
-          menuHome();
-        }}>홈</NavigationItem>
-        <NavigationItem id="navMember1" onClick={() => {
-          navigate("/Member1");
-          menuMember1();
-        }}>송규경</NavigationItem>
-        <NavigationItem id="navMember2" onClick={() => {
-          navigate("/Member2");
-          menuMember2();
-        }}>백채은</NavigationItem>
-        <NavigationItem id="navMember3" onClick={() => {
-          navigate("/Member3");
-          menuMember3();
-        }}>김하리</NavigationItem>
-        <NavigationItemMember4 id="navMember4" onClick={() => {
-          navigate("/Member4");
-          menuMember4();
-        }}>최유진</NavigationItemMember4>
-      </Navigation>
-    </Wrapper>
-    </Outerbox >
-  )
-}
+            ))}
+                </tbody>
+              </WrapperRIghtBodyBottomTable>
+            </WrapperRightBodyBottom>
+          </WrapperRightBody>
+        </WrapperRight>
+        <Navigation>
+          <NavigationItem id="navHome" onClick={() => {
+            navigate("/");
+            menuHome();
+          }}>홈</NavigationItem>
+          <NavigationItem id="navMember1" onClick={() => {
+            navigate("/Member1");
+            menuMember1();
+          }}>송규경</NavigationItem>
+          <NavigationItem id="navMember2" onClick={() => {
+            navigate("/Member2");
+            menuMember2();
+          }}>백채은</NavigationItem>
+          <NavigationItem id="navMember3" onClick={() => {
+            navigate("/Member3");
+            menuMember3();
+          }}>김하리</NavigationItem>
+          <NavigationItemMember4 id="navMember4" onClick={() => {
+            navigate("/Member4");
+            menuMember4();
+          }}>최유진</NavigationItemMember4>
+        </Navigation>
+      </Wrapper>
+      </Outerbox>
+      );
+};
 
-export default Member4;
 
-const Outerbox = styled.div`
-  width: 808px;
-  height: 544px;
-  background-image: url(${outerboxImage});
-`
+      export default Member4;
 
-const Wrapper = styled.div`
-  display:flex;
-  flex-direction: row;
-  padding: 32px 0px 0px 32px;
-`
+      const Outerbox = styled.div`
+      width: 808px;
+      height: 544px;
+      background-image: url(${outerboxImage});
+      `
 
-const WrapperLeft = styled.div`
-  width: 208px;
-  height: 472px;
-  display: flex;
-  flex-direction: column;
-`
+      const Wrapper = styled.div`
+      display:flex;
+      flex-direction: row;
+      padding: 32px 0px 0px 32px;
+      `
 
-const WrapperLeftHeader = styled.div`
-  width: 100%;
-  height: 30px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-`
+      const WrapperLeft = styled.div`
+      width: 208px;
+      height: 472px;
+      display: flex;
+      flex-direction: column;
+      `
 
-const Today = styled.div`
-  font-size: 9px;
-`
+      const WrapperLeftHeader = styled.div`
+      width: 100%;
+      height: 30px;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      `
 
-const TodayContents = styled.span`
-`
+      const Today = styled.div`
+      font-size: 9px;
+      `
 
-const WrapperLeftBody = styled.div`
-  width: 100%;
-  height: 100%;
-  border: 1px solid gray;
-  border-radius: 15px;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px 30px;
-`
+      const TodayContents = styled.span`
+      `
 
-const WrapperLeftBodyHeader = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-`
+      const WrapperLeftBody = styled.div`
+      width: 100%;
+      height: 100%;
+      border: 1px solid gray;
+      border-radius: 15px;
+      background-color: white;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 20px 30px;
+      `
 
-const GrayBox = styled.div`
-  background-image: url(${myphotoImage});
-  background-size: 148px 133px;
-  width: 148px;
-  height: 133px;
-`
+      const WrapperLeftBodyHeader = styled.div`
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      `
 
-const GrayLine1 = styled.div`
-  border-top: 1px dotted black;
-  margin: 12px 0px;
-`
+      const GrayBox = styled.div`
+      background-image: url(${myphotoImage});
+      background-size: 148px 133px;
+      width: 148px;
+      height: 133px;
+      `
 
-const WrapperLeftBodyProfile = styled.div`
-  width: 148px;
-  height: 150px;
-  display: flex;
-  flex-direction: column;
-  margin: 0px 0px 30px 0px;
-`
+      const GrayLine1 = styled.div`
+      border-top: 1px dotted black;
+      margin: 12px 0px;
+      `
 
-const ProfileLine = styled.div`
-  font-size: 5px;
-  font-weight: 400;
-  margin: 4px 0;
-`
+      const WrapperLeftBodyProfile = styled.div`
+      width: 148px;
+      height: 150px;
+      display: flex;
+      flex-direction: column;
+      margin: 0px 0px 30px 0px;
+      `
 
-const WrapperLeftBodyFooter = styled.div`
-  width: 148px; 
-  height: 50px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  font-size: 12px;
-  color: gray;
-`
+      const ProfileLine = styled.div`
+      font-size: 5px;
+      font-weight: 400;
+      margin: 4px 0;
+      `
 
-const MoodTitle = styled.div`
-`
+      const WrapperLeftBodyFooter = styled.div`
+      width: 148px;
+      height: 50px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      font-size: 12px;
+      color: gray;
+      `
 
-const MoodContents = styled.select`
-  width: 100%;
-  border: 1px solid gray;
-`
+      const MoodTitle = styled.div`
+      `
 
-const MoodContentsValue = styled.option`
-  width: inherit;
-  padding: 2px 10px 2px 5px;
-  border: 0 none;
-  font: gray;
-`
+      const MoodContents = styled.select`
+      width: 100%;
+      border: 1px solid gray;
+      `
 
-const WrapperRight = styled.div`
-  width: 524px;
-  height: 472px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  padding-left: 5px;
-`
+      const MoodContentsValue = styled.option`
+      width: inherit;
+      padding: 2px 10px 2px 5px;
+      border: 0 none;
+      font: gray;
+      `
 
-const WrapperRightHeader = styled.div`
-  width: 510px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: flex-end;
-  padding: 0px 14px;
-  margin: 0 0 5px 0;
-`
+      const WrapperRight = styled.div`
+      width: 524px;
+      height: 472px;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      padding-left: 5px;
+      `
 
-const WrapperRightHeaderTitle = styled.div`
-  color: #55B2E4;
-  font-size: 16px;
-`
+      const WrapperRightHeader = styled.div`
+      width: 510px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: flex-end;
+      padding: 0px 14px;
+      margin: 0 0 5px 0;
+      `
 
-const WrapperRightHeaderSetting = styled.div`
-  color: #55B2E4;
-  font-size: 9px;
-`
+      const WrapperRightHeaderTitle = styled.div`
+      color: #55B2E4;
+      font-size: 16px;
+      `
 
-const WrapperRightBody = styled.div`
-  width: 510px;
-  height: 445px;
-  border: 1px solid gray;
-  border-radius: 15px;
-  background-color: white;  
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between; 
-  padding: 10px 10px;
-`
+      const WrapperRightHeaderSetting = styled.div`
+      color: #55B2E4;
+      font-size: 9px;
+      `
 
-const WrapperRightBodyTop = styled.div`
-  width: 100%;
-  height: 210px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 7px 0;
-  margin-bottom: 40px;
-`
+      const WrapperRightBody = styled.div`
+      width: 510px;
+      height: 445px;
+      border: 1px solid gray;
+      border-radius: 15px;
+      background-color: white;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding: 10px 10px;
+      `
 
-const RightBodyTopTitle = styled.div`
-  width: 93%;
-  font-size: 11px;
-  font-weight: 600;
-  padding: 0 0 3px 0;
-`
+      const WrapperRightBodyTop = styled.div`
+      width: 100%;
+      height: 210px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 7px 0;
+      margin-bottom: 40px;
+      `
 
-const GrayLine2 = styled.div`
-  border-top: 0.9px solid gray;
-  width: 93%;
-  margin: 2px 0 6px 0;
-`
+      const RightBodyTopTitle = styled.div`
+      width: 93%;
+      font-size: 11px;
+      font-weight: 600;
+      padding: 0 0 3px 0;
+      `
 
-const RightBodyTopContents = styled.div`
-  width: 95%;
-  height: 80%;
-  display: flex;
-  flex-direction: column;
-  font-size: 8px;
-  padding: 0 5px;
-`
+      const GrayLine2 = styled.div`
+      border-top: 0.9px solid gray;
+      width: 93%;
+      margin: 2px 0 6px 0;
+      `
 
-const RightBodyTopContentsDescription = styled.div`
-  line-height: 1.3;
-`
+      const RightBodyTopContents = styled.div`
+      width: 95%;
+      height: 80%;
+      display: flex;
+      flex-direction: column;
+      font-size: 8px;
+      padding: 0 5px;
+      `
 
-const WrapperRightBodyBottom = styled.div`
-  width: 100%;
-  height: 190px;
-`
+      const RightBodyTopContentsDescription = styled.div`
+      line-height: 1.3;
+      `
 
-const WrapperRightBodyBottomTitle = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  padding: 1px 10px;
-`
+      const WrapperRightBodyBottom = styled.div`
+      width: 100%;
+      height: 190px;
+      `
 
-const TitleTitle = styled.div`
-  color: #55B2E4;
-  font-size: 12px;
-  font-weight: 700;
-`
+      const WrapperRightBodyBottomTitle = styled.div`
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 1px 10px;
+      `
 
-const TitleSubtitle = styled.div`
-  font-size: 7px;
-  font-weight: 500;
-  padding-left: 5px;
-`
+      const TitleTitle = styled.div`
+      color: #55B2E4;
+      font-size: 12px;
+      font-weight: 700;
+      `
 
-const WrapperRIghtBodyBottomTable = styled.table`
-  width: 100%;
-  margin-top: 5px;
-  border-spacing: 0;
-`
+      const TitleSubtitle = styled.div`
+      font-size: 7px;
+      font-weight: 500;
+      padding-left: 5px;
+      `
 
-const TableHeader = styled.th`
-  background-color: #eeeeee;
-  font-size: 10px;
-  font-weight: 700;
-  color: #999999;
-  border-top: 1px solid #999999;
-  border-bottom: 1px solid #999999;
-  padding-top: 3px;
-`
+      const WrapperRIghtBodyBottomTable = styled.table`
+      width: 100%;
+      margin-top: 5px;
+      border-spacing: 0;
+      `
 
-const TableData = styled.td`
-  border-bottom: 1px dotted #cbcbcc;
-  font-size: 10px;
-  font-weight: 400;
-  color: #0F465E;
-  padding-top: 4px;
-`
+      const TableHeader = styled.th`
+      background-color: #eeeeee;
+      font-size: 10px;
+      font-weight: 700;
+      color: #999999;
+      border-top: 1px solid #999999;
+      border-bottom: 1px solid #999999;
+      padding-top: 3px;
+     `
 
-const Navigation = styled.div`
-  width: 62px;
-  height: 100px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  position: absolute;
-  left: 774px;
-  top: 110px;
-`
+      const TableData = styled.td`
+      border-bottom: 1px dotted #cbcbcc;
+      font-size: 10px;
+      font-weight: 400;
+      color: #0F465E;
+      padding-top: 4px;
+      `
 
-const NavigationItem = styled.div`
-  width: 60px;
-  height: 30px;
-  background-color: #298eb5;
-  border: 1px solid black;
-  color: white;
-  border-radius: 0 10px 10px 0;
-  font-size: 10px;
-  text-align: center;
-  line-height: 30px;
-  cursor: pointer;
-  margin-bottom: 5px;
-`
+      const Navigation = styled.div`
+      width: 62px;
+      height: 100px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      position: absolute;
+      left: 774px;
+      top: 110px;
+      `
 
-const NavigationItemMember4 = styled.div`
-  background-color: white;
-  color: black;
-  width: 60px;
-  height: 30px;
-  border: 1px solid black;
-  border-radius: 0 10px 10px 0;
-  font-size: 10px;
-  text-align: center;
-  line-height: 30px;
-  cursor: pointer;
-  margin-bottom: 5px;
-`
+      const NavigationItem = styled.div`
+      width: 60px;
+      height: 30px;
+      background-color: #298eb5;
+      border: 1px solid black;
+      color: white;
+      border-radius: 0 10px 10px 0;
+      font-size: 10px;
+      text-align: center;
+      line-height: 30px;
+      cursor: pointer;
+      margin-bottom: 5px;
+      `
+
+      const NavigationItemMember4 = styled.div`
+      background-color: white;
+      color: black;
+      width: 60px;
+      height: 30px;
+      border: 1px solid black;
+      border-radius: 0 10px 10px 0;
+      font-size: 10px;
+      text-align: center;
+      line-height: 30px;
+      cursor: pointer;
+      margin-bottom: 5px;
+      `
